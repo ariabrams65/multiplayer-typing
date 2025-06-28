ef07947904aa8658171ae3c9c96581481eb7df7e
Ari Abrams <ariabrams65@gmail.com>
2025-06-28 08:20:44 -0700

    Added displaying of game info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,88 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [messages, setMessages] = useState([])
+  const [username] = useState(generateUsername());
+  const [prompt, setPrompt] = useState('');
+  const [countdown, setCountdown] = useState(null);
+  const [players, setPlayers] = useState([]);
+  const [connected, setConnected] = useState(false);
+
+  const ws = useRef(null);
+  const id = useRef(null);
+
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080/join?username=Jack');
-    ws.onmessage = (e) => {
-      setMessages((prev => [...prev, e.data]))
+    ws.current = new WebSocket(`ws://localhost:8080/join?username=${username}`);
+    ws.current.onopen = () => {
+      setConnected(true);
+    }
+    ws.current.onclose = () => {
+      setConnected(false);
+    }
+    ws.current.onmessage = (e) => {
+      handleMessage(JSON.parse(e.data));
     }
     return () => {
-      ws.close();
+      ws.current.close();
     }
-  }, []);
+  }, [username]);
+
+  function handleMessage(msg) {
+    switch (msg.type) {
+      case 'id':
+        id.current = msg.data.id;
+        break;
+      case 'prompt':
+        setPrompt(msg.data.text);
+        break;
+      case 'joined':
+        setPlayers(prev => {
+          return [...prev, {
+            id: msg.data.id,
+            username: msg.data.username,
+            index: 0,
+            wpm: 0
+          }];
+        });
+        break;
+      case 'removed':
+        setPlayers(prev => {
+          return prev.filter(player => {
+            player.id == msg.data.id
+          });
+        });
+        break;
+      case 'countdown':
+        setCountdown(msg.data.time);
+        break;
+      case 'progress':
+        break;
+    }
+  }
+
+
 
   return (
-    <ul>
-      {messages.map((msg, i) => (
-        <li key={i}>{msg}</li>
-      ))}
-    </ul>
+    <>
+      <p>Connected: {connected.toString()}</p>
+      <p>Username: {username}</p>
+      <p>Countdown: {countdown}</p>
+      <p>Prompt: {prompt}</p>
+      <ul>
+        {players.map(p => (
+          <li key={p.username}>
+            {p.username} - Index: {p.index} - WPM: {p.wpm}
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
-function Players() {
-  return (
-    <div>Players</div>
-  )
+function generateUsername() {
+  return 'User' + crypto.randomUUID();
 }
 
-function Prompt() {
-  return (
-    <div>Prompt</div>
-  )
-}
 
 
 export default App
