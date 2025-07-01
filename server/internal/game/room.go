feb4960b2f14ef7cc5a63bdf44de1678bd4eba16
Ari Abrams <ariabrams65@gmail.com>
2025-07-01 16:06:27 -0700

    Fetching quotes from api

--- server/internal/game/room.go
+++ server/internal/game/room.go
@@ -1,7 +1,10 @@
 package game
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -235,7 +238,23 @@ func (room *room) isProgressValid(id string, index int) bool {
 }
 
 func generatePrompt() string {
-	return "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do."
+	defaultPrompt := "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do."
+	res, err := http.Get("https://thequoteshub.com/api/")
+	if err != nil {
+		log.Print(err)
+		return defaultPrompt
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Print(err)
+		return defaultPrompt
+	}
+	obj := struct {
+		Text string `json:"text"`
+	}{}
+	json.Unmarshal([]byte(body), &obj)
+	return obj.Text
 }
 
 func calculateWpm(characters int, duration float64) float64 {
