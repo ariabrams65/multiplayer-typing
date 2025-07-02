dde3b59f9fe38835d642eb099da618c6fd8eafce
Ari Abrams <ariabrams65@gmail.com>
2025-07-02 16:17:41 -0700

    Added class for finished

--- client/src/components/PlayerList.jsx
+++ client/src/components/PlayerList.jsx
@@ -1,10 +1,10 @@
 import styles from "./PlayerList.module.css"
 
-export default function PlayerList({ players }) {
+export default function PlayerList({ players, playerFinished }) {
   return (
     <ol id={styles['player-list']}>
       {players.map(p => (
-        <li key={p.id} className={`${styles.player} ${p.removed ? styles.removed : ''}`} style={{ backgroundColor: p.color }}>
+        <li key={p.id} className={`${styles.player} ${p.removed ? styles.removed : ''} ${playerFinished(p.id) ? styles.finished : ''}`} style={{ backgroundColor: p.color }}>
           <span>{p.username}</span>
           <span>{Math.round(p.wpm)} WPM</span>
         </li>
