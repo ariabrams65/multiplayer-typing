885163cc951a3dfc8b7e8236ac72bb96778f5365
Ari Abrams <ariabrams65@gmail.com>
2025-07-02 07:28:19 -0700

    Changed how player caret style is computed to add underlines

--- client/src/components/Prompt.jsx
+++ client/src/components/Prompt.jsx
@@ -8,7 +8,7 @@ export default function Prompt({ input, prompt, players, myId }) {
 
   for (let i = 0; i < firstDiff; i++) {
     chars.push(
-      <span key={`c-${i}`} className={`${styles.correct} ${styles.caret}`} style={{ backgroundColor: getBackgroundColor(i)}}>
+      <span key={`c-${i}`} className={styles.correct} style={computeStyle(i)}>
         {prompt[i]}
       </span>
     );
@@ -25,14 +25,40 @@ export default function Prompt({ input, prompt, players, myId }) {
 
   for (let i = firstDiff; i < prompt.length; i++) {
     chars.push(
-      <span key={`u-${i}`} className={`${styles.pending} ${styles.caret}`} style={{ backgroundColor: getBackgroundColor(i)}}>
+      <span key={`u-${i}`} className={styles.pending} style={computeStyle(i)}>
         {prompt[i]}
       </span>
     );
   }
 
-  function getBackgroundColor(index) {
-    let color = '#ffffff00';
+  function computeStyle(i) {
+    let style;
+    if (isMyCaret(i)) {
+      style = {
+        "border-radius": "2px",
+        backgroundColor: getPlayersColor(i)
+      };
+    } else if (isOtherPlayersCaret(i)) {
+      style = {
+        "text-decoration": "underline",
+        "text-decoration-color": getPlayersColor(i)
+      };
+    } else {
+      style = null;
+    }
+    return style;
+  }
+
+  function isMyCaret(i) {
+    return players.some(p => p.id === myId && p.index === i);
+  }
+
+  function isOtherPlayersCaret(i) {
+    return players.some(p => p.id !== myId && p.index === i);
+  }
+
+  function getPlayersColor(index) {
+    let color = null;
     for (const p of players) {
       if (p.index === index) {
         if (p.id === myId) {
